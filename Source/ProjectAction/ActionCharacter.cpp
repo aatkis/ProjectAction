dd7caef06dd4a9ad959fd8d15220fe59f72526a3
Alex <coolbutterhunter@gmail.com>
2026-01-18 15:55:25 -0500

    Movement and Assets
    - Added movement and look function
    - Added Assets

--- Source/ProjectAction/ActionCharacter.cpp
+++ Source/ProjectAction/ActionCharacter.cpp
@@ -3,12 +3,25 @@
 
 #include "ActionCharacter.h"
 
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 
 // Sets default values
 AActionCharacter::AActionCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->bUsePawnControlRotation = true;
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComponent->SetupAttachment(SpringArmComp);
+	
+	
 }
 
 // Called when the game starts or when spawned
@@ -17,6 +30,7 @@ void AActionCharacter::BeginPlay()
 	Super::BeginPlay();
 	
 }
+
 
 // Called every frame
 void AActionCharacter::Tick(float DeltaTime)
@@ -28,5 +42,36 @@ void AActionCharacter::Tick(float DeltaTime)
 void AActionCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	
+	EnhancedInput->BindAction(InputMove, ETriggerEvent::Triggered, this, &AActionCharacter::Move);
+	EnhancedInput->BindAction(InputLook, ETriggerEvent::Triggered, this, &AActionCharacter::Look);
+	EnhancedInput->BindAction(InputJump, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+}
+
+void AActionCharacter::Move(const FInputActionValue& InValue)
+{
+	FVector2D InputValue = InValue.Get<FVector2D>();
+	
+	FVector MoveDir = FVector(InputValue.X, InputValue.Y, 0.0f);
+	
+	FRotator ControlRot = GetControlRotation();
+	ControlRot.Pitch = 0.0f;
+	
+	// Forward/Back dir
+	AddMovementInput(ControlRot.Vector(), InputValue.X);
+	
+	// Left/Right dir
+	FVector RightDir = ControlRot.RotateVector(FVector::RightVector);
+	AddMovementInput(RightDir, InputValue.Y);
+}
+
+void AActionCharacter::Look(const FInputActionInstance& InValue)
+{
+	FVector2D InputValue = InValue.GetValue().Get<FVector2D>();
+	
+	AddControllerPitchInput(InputValue.Y);
+	AddControllerYawInput(InputValue.X);
 }
 
--- Source/ProjectAction/ActionCharacter.h
+++ Source/ProjectAction/ActionCharacter.h
@@ -6,6 +6,12 @@
 #include "GameFramework/Character.h"
 #include "ActionCharacter.generated.h"
 
+struct FInputActionInstance;
+struct FInputActionValue;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class PROJECTACTION_API AActionCharacter : public ACharacter
 {
@@ -16,8 +22,29 @@ public:
 	AActionCharacter();
 
 protected:
+	
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> InputMove;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> InputLook;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> InputJump;
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	TObjectPtr<UCameraComponent> CameraComponent; 
+	
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	TObjectPtr<USpringArmComponent>SpringArmComp;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	void Move(const FInputActionValue& InValue);
+	
+	void Look(const FInputActionInstance& InValue);
 
 public:
 	// Called every frame
