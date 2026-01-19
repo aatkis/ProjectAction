3711ae237548ea9a9f2b4e8d6fd7c4998790f115
Alex <coolbutterhunter@gmail.com>
2026-01-18 23:25:14 -0500

    Magic
    - Added magic projectile

---
+++ Source/ProjectAction/Projectiles/ActionProjectileMagic.cpp
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActionProjectileMagic.h"
+#include "ActionProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+// Sets default values
+AActionProjectileMagic::AActionProjectileMagic()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.0f);
+	SphereComponent->SetCollisionProfileName("Projectile");
+	
+	LoopedNiagaraComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
+	LoopedNiagaraComp->SetupAttachment(SphereComponent);
+	
+	ProjectileMoveComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMoveComp"));
+	ProjectileMoveComp->InitialSpeed = 2000.0f;
+	ProjectileMoveComp->ProjectileGravityScale = 0.0f;
+}
+
+
+
---
+++ Source/ProjectAction/Projectiles/ActionProjectileMagic.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ActionProjectileMagic.generated.h"
+
+class UProjectileMovementComponent;
+class USphereComponent;
+class UNiagaraComponent;
+
+UCLASS()
+class PROJECTACTION_API AActionProjectileMagic : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AActionProjectileMagic();
+
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<UNiagaraComponent> LoopedNiagaraComp;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMoveComp;
+	
+	
+};
